129d9dfe915ee523aaedd0b03a43a45e381a8cec
João Victor Barros <joaovictor107543210@hotmail.com>
2022-09-23 21:23:12 -0300

    update pilha.java

--- notacaoPosFixa/src/notacaoPosFixa/Pilha.java
+++ notacaoPosFixa/src/notacaoPosFixa/Pilha.java
@@ -9,22 +9,27 @@ public class Pilha{
         this.posicao = -1;
     }
 	
-	
-	public boolean vazio(){
-        return this.posicao == -1;
-    }
-	 
-	public int getTamanho() {
-        return this.posicao+1;
-    }
-	
 	public Object inserirValor(Object valor) {
 		this.array[this.posicao+1] = valor;
 		this.posicao++;
 		
 		return this.array;
     }
-	 
+	
+	public Object ultimo(){
+        if(!vazio()){ 
+        	return this.array[this.posicao];
+    	}
+        return null; 
+	}
+	
+	public Object penultimo(){
+        if(!vazio()){ 
+        	return this.array[this.posicao - 1];
+    	}
+        return null; 
+    }
+	
 	public Object pop(){
         if(!vazio()){
         	
@@ -46,32 +51,12 @@ public class Pilha{
         return null; 
     }
 	
+	public boolean vazio(){
+        return this.posicao == -1;
+    }
 	 
-	public Object penultimo(){
-        if(!vazio()){ 
-        	return this.array[this.posicao - 1];
-    	}
-        return null; 
+	public int getTamanho() {
+        return this.posicao+1;
     }
- 
-	public Object ultimo(){
-        if(!vazio()){ 
-        	return this.array[this.posicao];
-    	}
-        return null; 
-	}
 	
-    public String converteString() {
-        String retorno  = "";
-        
-        for(int i = 0; i < this.posicao+1; i++){
-        	retorno += (this.array[i]);
-        	if(i < this.posicao) {
-        		retorno += (", ");        		
-        	}
-        	
-        }
-
-        return retorno;
-    }
 }
